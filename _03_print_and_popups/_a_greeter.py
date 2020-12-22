bdea851cdc5ddebe2078273de352864f50356198
root <root@root.com>
2020-12-22 22:34:14 +0000

    error on mad libs

--- _03_print_and_popups/_a_greeter.py
+++ _03_print_and_popups/_a_greeter.py
@@ -4,14 +4,17 @@ from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
     
     # Make a new window variable, window = Tk()
-    
+    w = Tk()
     # Hide the window using the window's .withdraw() method
-    
+    w.withdraw()
     # Ask the user for their name and save it to a variable
-    # name = simpledialog.askstring(title='Greeter', prompt="What is your name?")
+
+    #
+    name = simpledialog.askstring(title='Greeter', prompt="What is your name?")
     
     # Show a message box with your message using the .showinfo() method
-    
+    messagebox.showinfo(message='hi'+name)
     # Print your message to the console using the print() function
-    
-    # Show an error message using messagebox.showerror()
+    print('hello'+name)
+    # Show an error message using
+    messagebox.showerror(message="error")
--- _03_print_and_popups/_b_mad_libs.py
+++ _03_print_and_popups/_b_mad_libs.py
@@ -4,33 +4,33 @@ from tkinter import messagebox, simpledialog, Tk
 if __name__ == '__main__':
     
     # Make a new window variable, window = Tk()
-    
+    w = Tk()
     # Hide the window using the window's .withdraw() method
-
+    w.withdraw()
     # Put this sentence in a pop-up message box:
-    # "If you find yourself having to cross a piranha-infested river, here's how to do it..."
-    
+    # simpledialog.askstring(prompt="If you find yourself having to cross a piranha-infested river, here's how to do it...")
+    messagebox.showinfo(message="If you find yourself having to cross a piranha-infested river, here's how to do it...")
     # Get the player to enter an adjective
-     
+    adjective = simpledialog.askstring(title= "",prompt='enter adjective')
     # Get the player to enter a type of liquid
-    
+    type_of_liquid = simpledialog.askstring(title= "",prompt='type of liquid')
     # Get the player to enter a body part
-     
+    body_part = simpledialog.askstring(title= "",prompt='body part')
     # Get the player to enter a verb
-     
+    verb = simpledialog.askstring(title= "",prompt='verb')
     # Get the player to enter a place
-    
+    place = simpledialog.askstring(title= "",prompt='place')
     # The story below has has been written as a group of Strings joined together by + signs.
     # The story contains place holders, indicated by [** **] which you need to replace with
     # the values entered by the player.
     # Hint:  You will need to add more + signs to join the variables to the other parts of the story.
         
-    story = (
-    "Piranhas are more [**adjective**] during the day, so cross the river at\n"
-    "night. Piranhas are attracted to fresh [**type of liquid**] and will most\n"
-    "likely take a bite out of your [**body part**] if you [**verb**]. Whatever\n"
+    story = messagebox.showinfo(
+    "Piranhas are more " + adjective + " during the day, so cross the river at\n"
+    "night. Piranhas are attracted to fresh" + type_of_liquid  + " and will most\n"
+    "likely take a bite out of your " + body_part + " if you " + verb + ". Whatever\n"
     "you do, if you have an open wound, try to find another way to get "
-    "back to the [**place**]. Good luck!"
+    "back to the " + place + ". Good luck!"
     )
 
     
